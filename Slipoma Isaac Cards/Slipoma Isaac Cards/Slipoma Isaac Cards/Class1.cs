560c32837e6c0f96bf4cc09d442b369ff18d4783
slipoma <94129094+slipoma@users.noreply.github.com>
2022-02-13 21:00:13 +0100

    f

---
+++ Slipoma Isaac Cards/Slipoma Isaac Cards/Slipoma Isaac Cards/Class1.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using SlipomaIsaacCards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+
+namespace SlipomaIsaacCards
+
+{
+    [BepInDependency("com.willis.rounds.unbound",BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.modding.utils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+
+    [BepInPlugin(ModId, ModName, Version)]
+
+    [BepInProcess("Rounds.exe")]
+    public class Slipoma Isaac Cards : BaseUnityPlugin
+    public static SlipomaIsaacCards instance { get; private set; }
+    {
+ private const string ModId = "com.slipoma.rounds.SlipomaIsaacCards";
+    private const string ModName = "SlipomaIsaacCards";
+    private const string Version = "1.0.0";
+    public const string ModInitials = "SIC";
+
+    void Awake()
+    { var harmony = new Harmony(ModId);
+        harmony.PatchAll();
+    }
+    void Start(instance = this )
+    { 
+        CustomCard.BuildCard<EvasiveManeuvers>();
+    }
+  }
+}
+    
